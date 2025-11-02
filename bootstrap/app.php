9a8e31a61e60eab961be6d7a22a4e35bcb45534e
Peter <peter@example.com>
2025-11-02 21:14:21 +0100

    Fix HTTPS asset URLs on Heroku by configuring TrustProxies middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Register custom middleware aliases
+        // Trust proxies for Heroku and other reverse proxies
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
